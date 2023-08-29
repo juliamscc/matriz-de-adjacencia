abca0d2fd6d6f0764f7364c7535f40f9559f0033
julia <juliamscc@gmail.com>
2023-08-29 17:15:19 -0300

    inserindo grafo

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import networkx as nx
 G = nx.Graph()
 
-G.add_node(1)
-G.add_node(2)
+G.add_node('a')
+G.add_node('b')
+G.add_node('c')
+G.add_node('d')
+G.add_node('e')
 
 print(G.nodes())
 
-G.add_edge(1, 2)
+G.add_edge('a','b')
+G.add_edge('a','e')
+G.add_edge('b','c')
+G.add_edge('c','c')
 
 nVertices = G.number_of_nodes()
 nArestas = G.number_of_edges()
